484bdad573c8f1292a4b6075f149dca4e9812385
Luis Saavedra <luisdavidsag@gmail.com>
2025-04-18 15:29:26 -0500

    minor changes, fixing jekyll problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cv.ludsrill.com',
+  site: 'http://cv.ludsrill.com',
   outDir: 'docs',
   integrations: [react()]
 });
